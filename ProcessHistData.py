a9ecfecd47dec40e245dd7e4dcbb430aeec1d52b
Andrej <andrej.tupikin@googlemail.com>
2017-01-31 23:43:42 +0100

    first tries

--- ProcessHistData.py
+++ ProcessHistData.py
@@ -12,11 +12,29 @@ from googlefinance import getQuotes
 from datetime import datetime
 from time import gmtime, strftime
 
-
 def ProcessHistData():
 
-    MnemonicData = pd.read_csv('...csv')
-
+    HistData = pd.read_csv('/Users/andrejtupikin/pythonfirststeps/GetHistStockData/Data/BMW.csv', skiprows=1)
+    start_string = int(str(HistData).find('a14'))
+    str_found = [m.start() for m in re.finditer('\n', str(HistData))]
+    
+    array = [0 for y in range(0,len(str_found)-9)]
+    
+    for x in range(9, len(str_found)):
+        
+        delta_string = str(HistData)[str_found[x-1]+1:str_found[x]]
+        
+        start = delta_string.find(',')
+        
+        array[x-9] = np.appendfloat(delta_string[start+1:-1])
+        
+        print float(delta_string[start+1:-1])
+        
+    plt.plot(array,'r')
+    plt.show()
+        
+    #print string[166:-1].find("\n")
+    #print str_found[166:166+38]
 
 if __name__ == "__main__":
     ProcessHistData()
