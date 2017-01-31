2f4aa74d5a28552c4b37c8076a794ce6ac5bdecc
Andrej <andrej.tupikin@googlemail.com>
2017-01-31 21:37:06 +0100

    First commit

---
+++ ProcessHistData.py
@@ -0,0 +1,24 @@
+import urllib
+import requests
+import time, threading, sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mt
+import json
+import os
+import regex as re
+from googlefinance import getQuotes
+from datetime import datetime
+from time import gmtime, strftime
+
+
+def ProcessHistData():
+
+    MnemonicData = pd.read_csv('...csv')
+
+
+if __name__ == "__main__":
+    ProcessHistData()
+    
+    
