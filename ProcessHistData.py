f824d6bb616f1586afdde44266ce83b1cf326866
Andrej <andrej.tupikin@googlemail.com>
2017-05-03 01:06:57 +0200

    improvements

--- ProcessHistData.py
+++ ProcessHistData.py
@@ -12,16 +12,18 @@ from googlefinance import getQuotes
 from datetime import datetime
 from time import gmtime, strftime
 
-def ProcessHistData():
-
-    HistData = pd.read_csv('/Users/andrejtupikin/pythonfirststeps/GetHistStockData/Data/AllData/15d_60s/BMW.csv', skiprows=1)
+def ProcessHistData(strCurrentMnemonic):
+    
+    strMnemonicDataPath = "/Users/andrejtupikin/pythonfirststeps/GetHistStockData/Data/AllData/15d_60s/" + strCurrentMnemonic + ".csv"
+    
+    HistData = pd.read_csv(strMnemonicDataPath, skiprows=1)
     start_string = int(str(HistData).find('a14'))
     str_found = [m.start() for m in re.finditer('\n', str(HistData))]
     str_found_dates = [m.start() for m in re.finditer('a14', str(HistData))]
     
     w, h = len(str_found_dates),511;
     
-    Matrix = [[0 for x in range(w)] for y in range(h)]
+    Matrix = [[0 for x in range(w)] for y in range(h)];
     
     for x in range(0,len(str_found_dates)):
     
@@ -39,8 +41,6 @@ def ProcessHistData():
         
         strFoundValuesStart = [m.start() for m in re.finditer(',', str(strCurDateToNextDate))]
         strFoundValuesEnd   = [m.start() for m in re.finditer('\n', str(strCurDateToNextDate))]
-        
-        #print int((strCurDateToNextDate)[strFoundValuesEnd[0]:strFoundValuesStart[1]]);
             
         for y in range(1,len(strFoundValuesStart)+1):
                 
@@ -56,18 +56,20 @@ def ProcessHistData():
             
             Matrix[intNumber][x] = intCurVal;
             
-        print [row[x] for row in Matrix]
+        #print [row[x] for row in Matrix]
                 
-    print Matrix
+    return Matrix
             
+
+def firststrategytry(maData):
     
+    print maData
     
-    #data = str(HistData)[str_found_dates[0]+12:str_found_dates[1]]
-        
-    #print string[166:-1].find("\n")
-    #print str_found[166:166+38]
 
 if __name__ == "__main__":
-    ProcessHistData()
     
+    strMnemonic = "BMW";
     
+    maCurrentMnemonicData = ProcessHistData(strMnemonic);
+    
+    firststrategytry(maCurrentMnemonicData);
